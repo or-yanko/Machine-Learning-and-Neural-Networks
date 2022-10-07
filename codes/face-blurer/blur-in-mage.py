87d5c025d84b5ab91fbb2217d2d3105492aabe2e
or yanko <54112968+or-yanko@users.noreply.github.com>
2022-10-07 21:50:12 +0300

    Create blur-in-mage.py

---
+++ codes/face-blurer/blur-in-mage.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+prototxt_path = "weights/deploy.prototxt.txt"
+model_path = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+
+model = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
+image = cv2.imread("father-and-daughter.jpg")
+h, w = image.shape[:2]
+
+kernel_width = (w // 7) | 1
+kernel_height = (h // 7) | 1
+
+blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+model.setInput(blob)
+output = np.squeeze(model.forward())
+for i in range(0, output.shape[0]):
+    confidence = output[i, 2]
+    if confidence > 0.4:
+        box = output[i, 3:7] * np.array([w, h, w, h])
+        start_x, start_y, end_x, end_y = box.astype(np.int)
+        face = image[start_y: end_y, start_x: end_x]
+        face = cv2.GaussianBlur(face, (kernel_width, kernel_height), 0)
+        image[start_y: end_y, start_x: end_x] = face
+cv2.imshow("image", image)
+cv2.waitKey(0)
+cv2.imwrite("image_blurred.jpg", image)
